286338a871cf63baf37a408baf2b86bad78eed63
DivyaChakk <lintamrinalshubham@outlook.com>
2020-11-07 11:19:45 +0000

    Test cases are modified test_user_comment_email_flow.py

--- tests/test_user_comment_email_flow.py
+++ tests/test_user_comment_email_flow.py
@@ -5,7 +5,7 @@ from src.helpers.comments_helper import Comment
 from src.utilities.email_validator_utility import validate_email
 
 
-username = "Delphi"
+username = "Delphine"
 user_id = None
 def test_search_user():
     global user_id
@@ -19,6 +19,7 @@ def test_serach_posts():
     global all_posts_by_user
     post = Post()
     all_posts_by_user = post.get_post_ids_by_user_id(user_id)
+    assert all_posts_by_user != None or len(all_posts_by_user) != 0,"No Post has been published by the user"
 
 @pytest.mark.depends(on=['test_serach_posts'])
 def test_search_comments():
@@ -28,11 +29,13 @@ def test_search_comments():
     comment = Comment()
     for postid in all_posts_by_user:
         all_comments = comment.get_comments_by_post_id(postid)
+        assert all_comments != None or len(all_comments) !=0, "No Comment Posted on the post of the user"
         for comm in all_comments:
             emails.append(comm['email'])
     all_emails = emails
 @pytest.mark.depends(on=['test_search_comments'])
 def test_validate_email_in_comments():
     global all_emails
+    assert all_emails != None or len(all_emails) !=0 , "No email found to evaluate"
     for email in all_emails:
-        validate_email(email)
+        assert validate_email(email) == True , "Not Valid Email"
