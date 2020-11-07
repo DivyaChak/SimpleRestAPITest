b929c6f417bf3d9e290c22682c6dfcfb2b3e944e
DivyaChakk <lintamrinalshubham@outlook.com>
2020-11-07 12:55:11 +0000

    chnaged the ApiUtility modules assert status function

--- src/utilities/api_call_utility.py
+++ src/utilities/api_call_utility.py
@@ -9,17 +9,14 @@ class ApiCallUtility(object):
     def __init__(self):
         self._env = os.environ.get('ENV', 'test')
         self._base_url = src.config.api_config.API_URLS[self._env]
+        self._url = None
 
     def assert_status_code(self):
-        assert self.status_code == self.expected_status_code, f"Bad Status code. Seached user/post/comment not present" \
-                                                              f"Expected {self.expected_status_code}, Actual status " \
-                                                              f"code: {self.status_code}," \
-                                                              f"URL: {self.url}"
+        assert self.status_code == self.expected_status_code, f"Bad Status code. Seached user/post/comment not present Expected {self.expected_status_code}, Actual status code: {self.status_code},URL: {self._url}"
     def get(self, endpoint=None, payload=None, headers=None, expected_status_code=200):
         if not headers:
             headers = {"Content-Type": "application/json"}
-
-        self.url = self._base_url + endpoint
+        self._url = self._base_url + endpoint
         rs_api = requests.get(url=self.url, data=json.dumps(payload), headers=headers)
         self.status_code = rs_api.status_code
         self.expected_status_code = expected_status_code
