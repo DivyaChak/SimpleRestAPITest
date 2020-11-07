c60a8b3099ede15d96800f7d5447a52e6157d7bb
DivyaChakk <lintamrinalshubham@outlook.com>
2020-11-07 09:01:51 +0000

    Updated posts_helper.py module - changed the methods "get_post_ids_by_user_id" initially it was returing all post with all the details, now only returns the postid instead of all the unwanted details

--- src/helpers/posts_helper.py
+++ src/helpers/posts_helper.py
@@ -1,4 +1,6 @@
 from src.utilities.api_call_utility import ApiCallUtility
+
+
 # import logging as logger
 # User class which contains all the variables and methods related to Post
 
@@ -9,7 +11,10 @@ class Post(object):
         self._endpoint = None
 
     # This function is for getting all the post posted by a user using userId of the user
-    def get_posts_by_user_id(self, user_id):
+    def get_post_ids_by_user_id(self, user_id):
         self._endpoint = "posts?userId=" + str(user_id)
         posts = self.api_call_utility.get(self._endpoint)
-        return posts
+        post_ids = []
+        for post in posts:
+            post_ids.append(int(post['id']))
+        return post_ids
